eef5e6443391bdd3e00c79ac2707ca24ffa031ad
mkukiro <68143381+mkukiro@users.noreply.github.com>
2023-06-26 20:06:38 +0200

    🚚 Create `colors.kt`

---
+++ src/main/kotlin/api/colors.kt
@@ -0,0 +1,15 @@
+enum class Color(private val Code: String) {
+    RESET("\u001B[0m"),
+    BLACK("\u001B[30m"),
+    RED("\u001B[31m"),
+    GREEN("\u001B[32m"),
+    YELLOW("\u001B[33m"),
+    BLUE("\u001B[34m"),
+    MAGENTA("\u001B[35m"),
+    CYAN("\u001B[36m"),
+    WHITE("\u001B[37m");
+
+    override fun toString(): String {
+        return escapeCode
+    }
+}
