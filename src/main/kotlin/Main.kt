bf69a5962a25c308661236f0f5cca1d1a963ed51
mkukiro <68143381+mkukiro@users.noreply.github.com>
2023-06-26 17:14:42 +0200

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,14 +5,13 @@ var version = "v0.1"
 fun main() {
     println("fs.kotlin $version")
 
-    var isLooping = true
     while (isLooping) {
         var input = readln()
 
         when (input) {
             "help" -> help(version)
-            "exit" -> isLooping = false
-            else -> println("KTFS01: $input not found. type \"help\" for help.")
+            "exit" -> break
+            else   -> println("KTFS01: $input not found. type \"help\" for help.")
         }
     }
 }
