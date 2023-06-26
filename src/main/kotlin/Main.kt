2321f7091ac2e106a843a082faa9a0e45fe14237
mkukiro <68143381+mkukiro@users.noreply.github.com>
2023-06-26 18:43:36 +0200

    ✨ Added cursor

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,11 +2,13 @@ import commands.about
 import commands.help
 
 var version = "v3.0.0.1.od"
+var cursor  = "~>"
 
 fun main() {
     println("fs.kotlin $version")
 
     while (true) {
+        print("\u001B[32m" + cursor + "\u001B[0m")
         var input = readln()
 
         when (input) {
