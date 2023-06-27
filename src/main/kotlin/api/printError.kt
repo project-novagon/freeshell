73870deafb9c6e9dfc9d6ee54d59b0b4a28023f2
iDevYT <alphangred57@gmail.com>
2023-06-27 19:55:11 +0200

    :pencil: added question system

--- src/main/kotlin/api/printError.kt
+++ src/main/kotlin/api/printError.kt
@@ -4,3 +4,5 @@ import ANSIHeaders
 fun printConsoleError(code:String, input: String) {
     println("${ANSIHeaders.RED}$code${ANSIHeaders.RESET}: $input not found. type \"fs help\" for help.")
 }
+// TODO: Question System
+// println("${ANSIHeaders.CYAN} ? Are you sure? (${ANSIHeaders.GREEN}y${ANSIHeaders.CYAN}/${ANSIHeaders.RED}${ANSIHeaders.BOLD}N${ANSIHeaders.CYAN})${ANSIHeaders.RESET}")
