bcfc4370d73723ff091b73d43011dfffa1f4cf2b
iDevYT <alphangred57@gmail.com>
2023-07-01 14:54:09 +0200

    :recycle: some refactoring

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -35,9 +35,9 @@ fun main(args: Array<String>){
     if (fshLinuxExists == false || !freeshellLinuxPath.isDirectory || fshWindowsExists == false || !freeshellWindowsPath.isDirectory) {
         println("${ANSIHeaders.YELLOW} WARN: ${ANSIHeaders.RESET}Freeshell Config not found. Continuing with setup.")
         Thread.sleep(2000)
-        clear()
         println("${ANSIHeaders.BOLD} Freeshell Setup ${ANSIHeaders.RESET}")
         println("---")
+
         //TODO: make the rest of the setup
         //curl -OJLs https://github.com/project-novagon/fpm/releases/download/v1.2.0/fpm.py
     } else {
